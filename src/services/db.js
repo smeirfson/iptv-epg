9c697c244dfbf401ba81c69e3e245bd2ceca2831
Meir Fadida <meir.fadida@evolven.com>
2026-03-24 20:15:42 +0200

    Update DB connection for render

--- src/services/db.js
+++ src/services/db.js
@@ -1,7 +1,11 @@
 const { Pool } = require("pg");
 
+const isProduction = process.env.NODE_ENV === "production";
+
 const pool = new Pool({
     connectionString: process.env.DATABASE_URL,
+    ssl: isProduction
+        ? { rejectUnauthorized: false }
+        : false,
 });
-
 module.exports = { pool };
